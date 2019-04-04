69eae7cf669462b0fb98906c4a436090ae210011
jontenthoff <jontenthoff@hotmail.com>
2019-04-04 17:15:04 +0000

    Done.

--- lib/exercise.rb
+++ lib/exercise.rb
@@ -3,10 +3,26 @@ class Excecise
   
   @@all = []
   
-  def initialize(greeting)
-    puts "What exercise are we crushing today?"
+  def initialize(path)
+    
     
   end
   
+  def call
+    puts "Welcome to your simple exercise calculator!"
+    
+    puts "To list exercises, enter 'list excercises.'"
+    
+    puts "To quit, enter 'quit.'"
+    
+    while input != 'quit'
+    
+    puts "What exercise are we crushing today?"
+    input = gets.strip
+    
+    if input == 'list excercises'
+      list_exercises
+    end
+  end
 end
     
