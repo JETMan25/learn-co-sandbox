cc7e14cb15dbc1c563cd2ab364da0746ee2e772b
jontenthoff <jontenthoff@hotmail.com>
2019-04-04 17:21:52 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,11 @@ require 'pry'
 class Scraper
   
   def self.scrape.index.page(index_url)
-  
+   exercise = []
+    html = open(index_url)
+    index = Nokogiri::HTML(html)
+    
+    
   end
 
 end
