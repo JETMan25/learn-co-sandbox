e16d5973121bc872bde219d1f5a6d02fe826412d
jontenthoff <jontenthoff@hotmail.com>
2019-04-04 17:05:03 +0000

    Done.

--- lib/exercise.rb
+++ lib/exercise.rb
@@ -2,3 +2,11 @@ class Excecise
   attr_accessor :name
   
   @@all = []
+  
+  def initialize(greeting)
+    puts "What exercise are we crushing today?"
+    
+  end
+  
+end
+    
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,2 +1,12 @@
+require 'open-uri'
+require 'pry'
 
+class Scraper
+  
+  def self.scrape.index.page(index_url)
+  
+  end
+
+end
+  
 
